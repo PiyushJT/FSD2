2369f334974f57d7e7d2d05c98728bca368e1b36
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-03-20 11:46:53 +0530

    Refactor event handling for shape parameters

--- T1/NODE/e6.js
+++ T1/NODE/e6.js
@@ -2,20 +2,27 @@
 const e = require("events");
 
 const ee = new e();
+ee.on("message", (msg) => {
+            console.log(msg);
+        });
 ee.on("circle", (radius) => {
-    if (radius < 0) {
-        console.log("radius must be positive");
+    
+    if (radius < 0) {        
+        ee.emit("message", "radius must be positive");
     } else {
         const parameter = 2 * Math.PI * radius;
-        console.log(`parameter of the circle: ${parameter}`);
+       
+        ee.emit("message", `parameter of the circle: ${parameter}`);
     }
 });
 ee.on("square", (side) => {
+   
     if (side < 0) {
-        console.log("side must be positive");
+        ee.emit("message", "side must be positive");
     } else {
         const parameter = 4 * side;
-        console.log(`parameter of the square: ${parameter}`);
+       
+        ee.emit("message", `parameter of the square: ${parameter}`);
     }
 }); 
 
