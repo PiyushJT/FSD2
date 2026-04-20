4ed2e5c963620d1d6f7bdc80cc69907fc1f6183e
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-04-20 10:13:19 +0530

    Create cookie3.js

---
+++ T2/EXPRESS/cookie/cookie3.js
@@ -0,0 +1,14 @@
+// PB 200 : You have been assigned to develop a user feedback form for a website
+// using Express.js and cookies. Implement the following requirements:
+//  Process a form with the following fields: Name, Email , Message , Rating
+// (radio buttons: Bad, Average, Good, Very Good, Excellent) When the user
+// submits the form, store their feedback information (name, email, message,
+// and rating) in a cookie named "feedback" that expires in 10 seconds.
+// Display a confirmation message to the user after successfully submitting the
+// form & Create a link to display the feedback details stored in the "feedback"
+// cookie. When the user click to the link, retrieve the feedback information
+// from the cookie and display it on the page also include a link on the
+// feedback details page to Logout. When the user clicks the link, user
+// redirected to home page. Make app.js file use get method in express js. No
+// need to write html file having form elements.
+// After 10 seconds it will give message "no feedback available" message to user
