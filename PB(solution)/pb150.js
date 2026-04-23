99785f4fa51efa52758545db7892eec1d752d704
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-04-23 21:29:37 +0530

    Delete PB(solution)/pb150.js

--- PB(solution)/pb150.js
+++
@@ -1,8 +0,0 @@
-// write an express js to link html , css and js file and show one image
-// o.jpg. And also show the description of image. 
-const express = require('express');
-const app = express();
-app.use(express.static(__dirname,{index:'pb150.html'}));
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-});
