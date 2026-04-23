a4a623bff828b1b6ad51da7687a6d1abc07b5bcc
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-04-23 21:32:15 +0530

    Delete PB(solution)/pb158.js

--- PB(solution)/pb158.js
+++
@@ -1,8 +0,0 @@
-// Write an express.js script to print "The Pacific Ocean is the largest
-// and deepest of the world ocean" also run on appropriate localhost.
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send("The Pacific Ocean is the largest and deepest of the world ocean");
-});
-app.listen(3000)
