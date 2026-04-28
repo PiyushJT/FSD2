8060aff5a4672781b68045406b53db51dbbeb7d0
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-04-28 23:16:03 +0530

    Add files via upload

---
+++ PB(solution)/CH:6/pb229.js
@@ -0,0 +1,20 @@
+//  Write a code to upload file and access uploaded file in express js.
+const express = require('express');
+const multer = require('multer');
+const app = express();
+var store= multer.diskStorage({
+    destination:"LJU123",
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }   
+})
+var upload = multer({storage:store})
+app.get('/',(req,res)=>{
+                res.send("<form action='/upload' method='post' enctype='multipart/form-data'><input type='file' name='file'><button type='submit'>Upload</button></form>")
+})
+app.post('/upload',upload.single('file'),(req,res)=>{
+    res.send("File uploaded successfully")
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
