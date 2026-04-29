18b72e911d478c6ca7677661ef96a200d84ca019
Savaliya dwarkesh <mrdwarkesh65@gmail.com>
2026-04-29 11:54:41 +0530

    Delete T2/EXPRESS/EJS/views/ejs2.js

--- T2/EXPRESS/EJS/views/ejs2.js
+++
@@ -1,9 +0,0 @@
-express = require("express")
-app = express()
-app.set("view engine", "ejs")
-app.get("/", (req, res) => {
-    res.render("e2")
-})
-app.listen(3204, () => {
-    console.log("Server is running on port 3204")
-})
